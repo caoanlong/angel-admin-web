a0c0ef3e6a5c1ff532ab0ec69d6c45396d66ea80
Caoanlong <caoanlong@gmail.com>
2018-07-05 18:52:24 +0800

    gg

--- src/components/System/Role/components/AuthConfig.vue
+++ src/components/System/Role/components/AuthConfig.vue
@@ -4,7 +4,7 @@
 			:data="menus"
 			show-checkbox
 			default-expand-all
-			node-key="menuID"
+			node-key="menuId"
 			ref="tree"
 			highlight-current 
 			:default-checked-keys="selectedMenuId"
@@ -28,90 +28,7 @@ export default {
 			default: false
 		},
 		setRoleId: {
-			type: String,
-		}
-	},
-	data() {
-		return {
-			menus: [],
-			selectedMenuId: [],
-			defaultProps: {
-				children: 'children',
-				label: 'name'
-			}
-		}
-	},
-	watch: {
-		showSetAuth(newVal) {
-			// newVal && this.getAllMenus()
-			console.log(newVal)
-		}
-	},
-	methods: {
-		cancelAuth() {
-			this.$emit('selected-auth')
-		},
-		submitSetAuth() {
-			SysRole.addAuthority({
-				roleId: this.setRoleId,
-				menuIds: this.selectedMenuId
-			}).then(res => {
-				Message.success(res.data.msg)
-				this.$emit('selected-auth', true)
-			})
-		},
-		// getAllMenus() {
-		// 	console.log(this.setRoleId)
-		// 	SysRole.findMenuList({ roleId: this.setRoleId }).then(res => {
-		// 		this.menus = res.menuList
-		// 		this.selectedMenuId = res.menuIDList
-		// 	})
-		// },
-		selectMenu(data, isSelected) {
-			if (isSelected) {
-				this.selectedMenuId.push(data.menuId)
-			} else {
-				this.selectedMenuId.splice(this.selectedMenuId.indexOf(data.menuId), 1)
-			}
-		}
-	}
-}
-</script>
-
-<style lang="stylus" scoped>
-
-</style>
-<template>
-	<el-dialog title="权限设置" :visible.sync="showSetAuth" width="30%">
-		<el-tree
-			:data="menus"
-			show-checkbox
-			default-expand-all
-			node-key="menuID"
-			ref="tree"
-			highlight-current 
-			:default-checked-keys="selectedMenuId"
-			:props="defaultProps"
-			@check-change="selectMenu" style="height:400px;overflow-y:auto">
-		</el-tree>
-		<span slot="footer" class="dialog-footer">
-			<el-button @click="cancelAuth">取 消</el-button>
-			<el-button type="primary" @click="submitSetAuth">确 定</el-button>
-		</span>
-	</el-dialog>
-</template>
-
-<script>
-import { Message } from 'element-ui'
-import SysRole from '../../../../api/SysRole'
-export default {
-	props: {
-		showSetAuth: {
-			type: Boolean,
-			default: false
-		},
-		setRoleId: {
-			type: String,
+			type: String
 		}
 	},
 	data() {
@@ -135,24 +52,24 @@ export default {
 		},
 		submitSetAuth() {
 			SysRole.addAuthority({
-				roleID: this.setRoleId,
-				menuIDs: this.selectedMenuId
+				roleId: this.setRoleId,
+				menuIds: this.selectedMenuId
 			}).then(res => {
 				Message.success(res.data.msg)
 				this.$emit('selected-auth', true)
 			})
 		},
 		getAllMenus() {
-			SysRole.findMenuList({ roleID: this.setRoleId }).then(res => {
+			SysRole.findMenuList({ roleId: this.setRoleId }).then(res => {
 				this.menus = res.menuList
 				this.selectedMenuId = res.menuIds
 			})
 		},
 		selectMenu(data, isSelected) {
 			if (isSelected) {
-				this.selectedMenuId.push(data.menuID)
+				this.selectedMenuId.push(data.menuId)
 			} else {
-				this.selectedMenuId.splice(this.selectedMenuId.indexOf(data.menuID), 1)
+				this.selectedMenuId.splice(this.selectedMenuId.indexOf(data.menuId), 1)
 			}
 		}
 	}
